e02f7ceb9d5468e17a36c1129fca42d55f024210
StefanoBg <stefantobg@egmail.com>
2023-01-24 12:35:01 +0200

    ihuuuu

---
+++ src/content.js
@@ -0,0 +1,21 @@
+let content = [
+    {
+        title: "My new Outfit",
+        text:"Where is the text here? "
+    },
+
+    {
+        title: "My old Outfit",
+        text:"jeans and Jacket form fisherman  "
+    },
+    {
+        title: "My new Outfit",
+        text:"Where is the text here? "
+    },
+    {
+        title: "Angela Whooo",
+        text:"Where is the Chinesses ambasador?"
+    },
+]
+
+export default content;
